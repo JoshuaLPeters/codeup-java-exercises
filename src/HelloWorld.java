5e70b66a083024bba5a48e45a134c01cacbefa73
Joshua Peters <peters.l.joshua@gmail.com>
2019-11-20 13:33:54 -0600

    Syntax types and variables exercise complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,54 @@ public class HelloWorld {
     public static void main(String[] args) {
 //        System.out.println("Hello, World!");
         System.out.print("Hello,");
-        System.out.print(" World!");
+        System.out.println(" World!");
+
+        int myFavoriteNumber = 787;
+        System.out.println("My favorite number is: " + myFavoriteNumber);
+
+        String myString = "Mochi";
+        System.out.println("My first string is: " + "\"" + myString + "\"" + ", which is also my cat's name!");
+
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++); printing a number with the variable x plus a value
+//        System.out.println(x);   printing the initialization of the variable;
+
+//        int x = 5;
+//        System.out.println(++x); printing a number with the variable being changed
+//        System.out.println(x); printing the new value of the variable x
+
+//        String class;
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+//      Simulating an HTTP request
+        String ageParameter = "29";
+        byte realAge = (byte) (Integer.parseInt(ageParameter) +1);
+
+//        int x = 4;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//        y *= x;
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+
+
+
+
+
+
     }
 }
