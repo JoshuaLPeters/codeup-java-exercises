b55c49961a7f03255e78546fe937f3264cb02400
Joshua Peters <peters.l.joshua@gmail.com>
2020-02-29 11:37:16 -0600

    practiced for loop with Mochi game

--- src/GraduatedExercises/Friday.java
+++ src/GraduatedExercises/Friday.java
@@ -8,6 +8,10 @@ public class Friday {
         System.out.println("Press 1 if Mochi is happy, press 2 if Mochi is sad :(");
     }
 
+    public static void minutesPlayed(){
+        System.out.println("Tell me how many minutes you want to play with Mochi!");
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -19,6 +23,14 @@ public class Friday {
             System.out.println("Yay, Mochi is so happy!");
         } else {
             System.out.println("Aww... Mochi is sad... Grab his stick thing and play with him!");
+        }
+
+        System.out.println("Okay, well now that we know Mochi's mood...");
+        minutesPlayed();
+        int minutes = sc.nextInt();
+
+        for (int i = 0; i < minutes; i++){
+            System.out.println("Meow!");
         }
     }
 }
